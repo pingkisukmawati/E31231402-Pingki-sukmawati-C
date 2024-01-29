7ab026de2dca28048a9569750eda54187f61a51b
pingkisukmawati <pingkisukmawati6@gmail.com>
2024-01-29 14:49:46 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ascii;
+public class main {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int chr = 'A';
+System.out.println("The ASCII value of A is :"+chr);
+        
+    }
+    
+}
